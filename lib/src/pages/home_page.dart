ad6844ee17e3c97e85632671d657acfda35153d0
militho <militho.blazitho@gmail.com>
2019-08-21 22:41:55 -0500

    Seccion 8, clase 150 terminada

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:qrreaderapp/src/pages/direcciones_page.dart';
 import 'package:qrreaderapp/src/pages/mapas_page.dart';
 
+import 'package:qrcode_reader/qrcode_reader.dart';
+
 class HomePage extends StatefulWidget {
 
   @override
@@ -15,9 +17,45 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('QR Scanner'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon( Icons.delete_forever ),
+            onPressed: (){},
+          )
+        ],
+      ),
       body: _callPage( currentIndex ),
       bottomNavigationBar: _crearBottomNavigationBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon( Icons.filter_center_focus ),
+        onPressed: _scanQR,
+        backgroundColor: Theme.of(context).primaryColor,
+      )
     );
+  }
+
+  _scanQR() async {
+
+    // https://www.google.com
+    // geo:40.68571377944765,-73.95512953242189
+
+    String futureString = '';
+
+    try{
+      futureString = await new QRCodeReader().scan();
+    } catch (error) {
+      futureString = error.toString();
+    }
+
+    print('futureString: $futureString');
+
+    if( futureString != null ) {
+      print('Tenemos informacion');
+    }
+
   }
 
   Widget _callPage( int paginaActual ){
